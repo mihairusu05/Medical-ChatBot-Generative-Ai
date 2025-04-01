ec2e04dccf26be47f374c32c29e685700d47ab95
Mihai Rusu <mihai10rusu@gmail.com>
2025-04-01 20:00:06 +0300

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Mihai Rusu',
+    author_email = 'mihai10rusu@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
