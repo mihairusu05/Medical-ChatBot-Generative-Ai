8e7bce8456a15801a26914aab6585fca740e4262
Mihai Rusu <mihai10rusu@gmail.com>
2025-04-02 17:16:38 +0300

    Work in progress

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+    "You are an assistant for question-answering tasks."
+    "Use the following pieces of retrieved context to answer "
+    "the question .If you do not know the answer , say that you do not know." \
+    "Use three sentences maximum and keep the answers concise." \
+    "\n\n"
+    "{context}"
+)
